836f6a3217338e575a438f76c141a15db8068691
Shanen Cross <shanencross@yahoo.com>
2021-03-29 18:29:42 -0700

    Fix webpack configuration file project name

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'Shape Tracker',
+      title: 'Team-Week-Project-Jeopardy-Simulator',
       template: './src/index.html',
       inject: 'body'
     })
