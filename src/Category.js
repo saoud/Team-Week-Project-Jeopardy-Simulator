c866ff26785f7a8ab43ec6a5f77f656b5bb979b8
Shanen Cross <shanencross@yahoo.com>
2021-03-29 19:02:29 -0700

    Make basic Category class

---
+++ src/Category.js
@@ -0,0 +1,28 @@
+export default class Category {
+  constructor(categoryResponse) {
+    this.id = categoryResponse.id;
+    this.title = categoryResponse.title;
+    this.clues = makeClues(categoryResponse.clues);
+  }
+
+  makeClues(responseClues) {
+    let clues = {};
+    for (let responseClue of responseClues) {
+      if (responseClue.value === 200 || 
+          responseClue.value === 400 ||
+          responseClue.value === 600 ||
+          responseClue.value === 800 ||
+          responseClue.value === 1000) {
+        let newClue = {
+          id: responseClue.id,
+          answer: responseClue.answer,
+          question: responseClue.question,
+          value: responseClue.value
+        };
+        
+        clues[responseClue.value] = newClue;
+      }
+    }
+    return clues;
+  }
+}
