5e689788fdc14a11ee91f70e2c92800ed3905479
Shanen Cross <shanencross@yahoo.com>
2021-04-07 01:48:53 -0700

    Change request looping so that when an invalid category is found, only replace that category, and none of the others found so far

--- src/main.js
+++ src/main.js
@@ -14,10 +14,10 @@ let playerTwo;
 
 function scoreboardShow() {
   $("#playerOneName").text(playerOne.name);
-  if (playerOne.score < 0) { $("#playerOneScore").html(`<div class="text-danger">${playerOne.score}<div>`) }
+  if (playerOne.score < 0) { $("#playerOneScore").html(`<div class="text-danger">${playerOne.score}<div>`); }
   else { $("#playerOneScore").text(playerOne.score); }
   $("#playerTwoName").text(playerTwo.name);
-  if (playerTwo.score < 0) { $("#playerTwoScore").html(`<div class="text-danger">${playerTwo.score}<div>`) }
+  if (playerTwo.score < 0) { $("#playerTwoScore").html(`<div class="text-danger">${playerTwo.score}<div>`); }
   else { $("#playerTwoScore").text(playerTwo.score); }
 }
 
@@ -53,22 +53,23 @@ function displayErrors(error) {
   $('.show-errors').text(`${error}`);
 }
 
-function getCategoryIds(list) {
-  let catIds = [];
-  let randomIndices = [];
-  while (randomIndices.length < 5) {
-    let randomIndex = Math.floor(Math.random() * 100);
-    if (randomIndices.indexOf(randomIndex) === -1) {
-      randomIndices.push(randomIndex);
-    }
-  }
-  console.log(randomIndices);
-  for (let randomIndex of randomIndices) {
-    catIds.push(list[randomIndex].id);
-  }
+// function getCategoryIds(list) {
+//   let catIds = [];
+//   let randomIndices = [];
+//   while (randomIndices.length < 5) {
+//     let randomIndex = Math.floor(Math.random() * 100);
+//     if (randomIndices.indexOf(randomIndex) === -1) {
+//       randomIndices.push(randomIndex);
+//     }
+//   }
 
-  return catIds;
-}
+//   console.log(randomIndices);
+//   for (let randomIndex of randomIndices) {
+//     catIds.push(list[randomIndex].id);
+//   }
+
+//   return catIds;
+// }
 
 function generateCategoryTitles() {
   for (let categoryIndex = 0; categoryIndex < categories.length; categoryIndex++) {
@@ -146,103 +147,59 @@ function makeCategoryList(categoryListResponse) {
   return categoryListResponse;
 }
 
-function makeRandomCategories(categoryListResponse) {
-  categories = [];
-  categoryIds = getCategoryIds(categoryListResponse);
-  console.log(categoryIds);
-  //where we put the catergories ids go
-  return CategoryLookupService.getCategory(categoryIds[0])
-    .then(function (categoryResponse1) {
-      if (categoryResponse1 === undefined) {
-        console.log("categoryResponse1 is undefined");
-        return;
+function getRandomId(categoryList) {
+  let randomId;
+  do {
+    let randomIndex = Math.floor(Math.random() * categoryList.length);
+    randomId = categoryList[randomIndex].id;
+  } while (categoryIds.includes(randomId));
+  categoryIds.push(randomId);
+  return randomId;
+}
+
+function getRandomCategory(categoryList) {
+  let categoryId = getRandomId(categoryList);
+  console.log(categoryId);
+  return CategoryLookupService.getCategory(categoryId)
+    .then(function(categoryResponse) {
+      if (categoryResponse instanceof Error) {
+        throw Error(`category API error: ${categoryResponse.message}`);
       }
-      if (categoryResponse1 instanceof Error) {
-        throw Error(`category API error: ${categoryResponse1.message}`);
+      console.log(`categoryResponse:`);
+      console.log(categoryResponse);
+      let category = new Category(categoryResponse);
+      if (category.clues === null || category.clues === undefined) {
+        console.log("Missing clue in category");
+        return getRandomCategory(categoryList);
       }
-      let category1 = new Category(categoryResponse1);
-      console.log(`categoryResponse1:`);
-      console.log(categoryResponse1);
-      if (category1.clues === null || category1.clues === undefined) {
-        console.log("Missing clue in category1");
-        return makeRandomCategories(categoryListResponse);
-      }
-      categories.push(category1);
-      return CategoryLookupService.getCategory(categoryIds[1]);
-    }).then(function (categoryResponse2) {
-      if (categoryResponse2 === undefined) {
-        console.log("categoryResponse2 is undefined");
-        return;
-      }
-      if (categoryResponse2 instanceof Error) {
-        throw Error(`category API error: ${categoryResponse2.message}`);
-      }
-      console.log(`categoryResponse2:`);
-      console.log(categoryResponse2);
-      let category2 = new Category(categoryResponse2);
-      if (category2.clues === null || category2.clues === undefined) {
-        console.log("Missing clue in category2");
-        return makeRandomCategories(categoryListResponse);
-      }
-      categories.push(category2);
-      return CategoryLookupService.getCategory(categoryIds[2]);
-    }).then(function (categoryResponse3) {
-      if (categoryResponse3 === undefined) {
-        console.log("categoryResponse3 is undefined");
-        return;
-      }
-      if (categoryResponse3 instanceof Error) {
-        throw Error(`category API error: ${categoryResponse3.message}`);
-      }
-      console.log(`categoryResponse3:`);
-      console.log(categoryResponse3);
-      let category3 = new Category(categoryResponse3);
-      if (category3.clues === null || category3.clues === undefined) {
-        console.log("Missing clue in category3");
-        return makeRandomCategories(categoryListResponse);
-      }
-      categories.push(category3);
-      return CategoryLookupService.getCategory(categoryIds[3]);
-    }).then(function (categoryResponse4) {
-      if (categoryResponse4 === undefined) {
-        console.log("categoryResponse4 is undefined");
-        return;
-      }
-      if (categoryResponse4 instanceof Error) {
-        throw Error(`category API error: ${categoryResponse4.message}`);
-      }
-      console.log(`categoryResponse4:`);
-      console.log(categoryResponse4);
-      let category4 = new Category(categoryResponse4);
-      if (category4.clues === null || category4.clues === undefined) {
-        console.log("Missing clue in category4");
-        return makeRandomCategories(categoryListResponse);
-      }
-      categories.push(category4);
-      return CategoryLookupService.getCategory(categoryIds[4]);
-    }).then(function (categoryResponse5) {
-      if (categoryResponse5 === undefined) {
-        console.log("categoryResponse5 is undefined");
-        return;
-      }
-      if (categoryResponse5 instanceof Error) {
-        throw Error(`category API error: ${categoryResponse5.message}`);
-      }
-      console.log(`categoryResponse5:`);
-      console.log(categoryResponse5);
-      let category5 = new Category(categoryResponse5);
-      if (category5.clues === null || category5.clues === undefined) {
-        console.log("Missing clue in category5");
-        return makeRandomCategories(categoryListResponse);
-      }
-      categories.push(category5);
+      categories.push(category);
     });
+}
+
+function getRandomCategories(categoryList) {
+  categories = [];
+  categoryIds = [];
+
+  return getRandomCategory(categoryList)
+    .then(() => getRandomCategory(categoryList))
+    .then(() => getRandomCategory(categoryList))
+    .then(() => getRandomCategory(categoryList))
+    .then(() => getRandomCategory(categoryList));
+  // return Promise.all([
+  //   getRandomCategory(categoryList),
+  //   getRandomCategory(categoryList),
+  //   getRandomCategory(categoryList),
+  //   getRandomCategory(categoryList),
+  //   getRandomCategory(categoryList)
+  // ]).then(function(randomCategories) {
+  //   categories = randomCategories;
+  // });
 }
 
 $(document).ready(function () {
   CategoriesListService.getCategoryList()
     .then(makeCategoryList)
-    .then(makeRandomCategories)
+    .then(getRandomCategories)
     .then(createBoard)
     .catch(function (error) {
       console.log(error);
